1abca7888bbff11a2c1dfd1e6721ffe9fbc845f7
manisaireddy2001 <145651482+manisaireddy2001@users.noreply.github.com>
2023-09-21 13:01:23 +0530

    Add files via upload

---
+++ Manisai/manisai.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+# Load and preprocess the MNIST dataset
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Define the CNN architecture
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(train_images, train_labels, epochs=5,
+                    validation_data=(test_images, test_labels))
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(f'Test accuracy: {test_acc}')
+
+# Plot training history
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
